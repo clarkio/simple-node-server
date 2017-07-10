37570e48e3e4f199f5ce8a6a27f95660df8116f7
Brian Clark <clarkio@users.noreply.github.com>
2017-07-10 11:18:49 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 4000;
 const runningMessage = 'Server is running on port ' + port;
 
 app.get('/', function (req, res){
