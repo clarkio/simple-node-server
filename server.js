d1569b06d3cdc740eeefc48cd7ff91d169006cda
clarkio <brian@clarkio.com>
2017-09-08 20:42:33 -0400

    use es6

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-
 const express = require('express');
 const app = express();
 const port = process.env.PORT;
 const runningMessage = 'Server is running on port ' + port;
 
-app.get('/', function (req, res){
-    console.log('API was successfully requested');
-    res.send(runningMessage);
+app.get('/', (req, res) => {
+  console.log('API was successfully requested');
+  res.send(runningMessage);
 });
 
-app.listen(port, function() {
-    console.log(runningMessage);
+app.listen(port, () => {
+  console.log(runningMessage);
 });
