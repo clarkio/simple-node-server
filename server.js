1f8e5a06fd6c5da6f30fe5d62b82d700b290cbce
clarkio <brian@clarkio.com>
2017-11-01 08:29:55 -0700

    remove query log

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 4000;
 const runningMessage = 'Server is running on port ' + port;
 
 app.get('/', (req, res) => {
-  console.log('API was successfully requested:', req.query);
+  console.log('API was successfully requested');
   res.send(runningMessage);
 });
 
